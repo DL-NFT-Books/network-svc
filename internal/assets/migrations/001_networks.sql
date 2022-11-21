3e6c164b08347bdc632bc4cb15a8ca6bababa5a7
slbmax <slobodzian.maxim@gmail.com>
2022-11-21 13:18:29 +0200

    added chain_id idx

--- internal/assets/migrations/001_networks.sql
+++ internal/assets/migrations/001_networks.sql
@@ -8,6 +8,8 @@ CREATE TABLE networks
     ws_url text NOT NULL DEFAULT '',
     factory_address VARCHAR(42) NOT NULL DEFAULT ''
 );
+CREATE UNIQUE INDEX idx_networks_chain_id ON networks (chain_id);
 
 -- +migrate Down
+DROP INDEX IF EXISTS idx_networks_chain_id;
 DROP TABLE networks;
